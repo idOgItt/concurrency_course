066f9cff629eaec303af71a700b4682651aba384
Sergey <97730330+idOgItt@users.noreply.github.com>
2026-07-27 21:35:23 +0300

    Portfolio snapshot: sync/lock_guard

---
+++ sync/lock_guard/lock_guard.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+template <typename Mutex>
+class LockGuard {
+ public:
+  explicit LockGuard(Mutex& mutex)
+      : mutex_(mutex) {
+    mutex_.lock();
+  }
+
+  ~LockGuard() {
+    mutex_.unlock();
+  }
+
+  // Non-copyable
+  LockGuard(const LockGuard&) = delete;
+  LockGuard& operator=(const LockGuard&) = delete;
+
+  // Non-movable
+  LockGuard(LockGuard&&) = delete;
+  LockGuard& operator=(LockGuard&&) = delete;
+
+ private:
+  Mutex& mutex_;
+};
